b2e87e0ee25dce83f5f4f469cecfdf4c9021ec62
ALX <alexmonac13@gmail.com>
2023-06-09 23:50:35 +0200

    [ Update ] users repos fetch dynamic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const search = document.querySelector("#search-bar");
 const submit = document.querySelector("#submit-btn");
 const output = document.querySelector("#output");
+let user;
 
 submit.addEventListener("click", () => {
   if (!search.value) {
-    alert("Veuillez bien reinseigner l'utilisateur");
+    alert("Veuillez bien renseigner l'utilisateur");
     console.error("Error");
     return;
   }
@@ -14,6 +15,7 @@ submit.addEventListener("click", () => {
   fetch(profile)
     .then((response) => response.json())
     .then((data) => {
+      user = data.login;
       console.table(data);
       const template = `
         <div id="result">
@@ -23,16 +25,28 @@ submit.addEventListener("click", () => {
             <a href="${data.html_url}" target="_blank">Profile</a>
             <span>${data.blog}</span>
             <span>${data.location}</span>
-            <span>${data.public_repos}</span>
+            <span>Nombre de repos publics : ${data.public_repos}</span>
             <span>${data.followers}</span>
             <span>${data.following}</span>
         </div>
       `;
       output.innerHTML = template;
-    });
 
-  const repos = "https://api.github.com/users/13vlxx/repos";
-  fetch(repos)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+      const repos = `https://api.github.com/users/${user}/repos`;
+      fetch(repos)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          const h1 = "<h1>Liste des repos</h1>";
+          output.innerHTML += h1;
+          data.forEach((repo) => {
+            const template = `
+            <div id="repos">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            `;
+            output.innerHTML += template;
+          });
+        });
+    });
 });
